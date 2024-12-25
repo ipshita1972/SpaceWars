9968beb75b1c9186b072a6ca7c8c115eeb7298d3
Ipshita Mandal <ipshitam46@gmail.com>
2024-12-25 08:47:31 +0530

    modified

--- client/src/contract/NFT_MarketPlace/index.js
+++ client/src/contract/NFT_MarketPlace/index.js
@@ -1,4 +1,4 @@
 import contract from "./NFT_MarketPlace.json";
 
-export const ADDRESS = "0x93E0Be08B940c5C9b10591ff8101D847DF1A4cf4";
+export const ADDRESS = "{contract address}";
 export const { abi: ABI } = contract;
--- client/src/contract/SpaceWars/index.js
+++ client/src/contract/SpaceWars/index.js
@@ -1,4 +1,4 @@
 import contract from "./SpaceWars.json";
 
-export const ADDRESS = "0xB6C2684B224e403FAdf5Ff9A2Df10991AA7FA4A0";
+export const ADDRESS = "{contract address}";
 export const { abi: ABI } = contract;
